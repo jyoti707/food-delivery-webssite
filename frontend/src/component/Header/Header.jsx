c3f4d858e8fa30c4e22c3091922af8f03fee5fbb
jyoti <jyoti734singh@gmail.com>
2025-11-03 22:52:33 +0530

    food delivery website

---
+++ frontend/src/component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Hungry? We’ve got you covered! From sizzling classics to trending favorites, TastyTrack delivers food that’s fresh, fast, and full of flavor — right to your doorstep.</p>
+            <button>View Menu</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
