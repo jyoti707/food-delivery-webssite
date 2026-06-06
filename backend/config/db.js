08b1a0270fc7ce199066ecd9c8110a8ac1f51f19
jyoti <jyoti734singh@gmail.com>
2026-06-06 18:57:27 +0530

    mongo atlas and render setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,11 @@
+// import mongoose from "mongoose";
+
+// export const connectDB=async()=>{
+//     await mongoose.connect('mongodb+srv://greatstack:jyoti123@cluster0.fnqwcks.mongodb.net/FOOD_WEBSITE').then(()=>console.log("DB connected"));
+// }
 import mongoose from "mongoose";
 
-export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://greatstack:jyoti123@cluster0.fnqwcks.mongodb.net/FOOD_WEBSITE').then(()=>console.log("DB connected"));
+export const connectDB = async () => {
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("DB connected"));
 }
